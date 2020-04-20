f852b2abca39b12a7d2b0e876bea0621f1f1d0b7
Rob Ranf <robranf@gmail.com>
2020-04-19 18:41:52 -0700

    first commit

---
+++ src/components/EventCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "EventCard",
+  props: {
+    propName: {
+      type: Number,
+      required: true,
+      default: 10
+    }
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style></style>
