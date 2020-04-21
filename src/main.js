828b6b94b7857aa8fef1eccbb7383320257e02a9
Rob Ranf <robranf@gmail.com>
2020-04-21 16:56:47 -0700

    adding base icon

--- src/main.js
+++ src/main.js
@@ -2,11 +2,53 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import upperFirst from "lodash/upperFirst";
+import camelCase from "lodash/camelCase";
+// import BaseIcon from "@/components/BaseIcon.vue";
+
+// globally register a component; two args, the name you want to use and the import reference from above...will usually be the same but don't have to be
+// Vue.component("BaseIcon", BaseIcon);
+
+// automatic global registration
+
+const requireComponent = require.context(
+  // The relative path of the components folder
+  "./components",
+  // Whether or not to look in subfolders
+  false,
+  // The regular expression used to match base component filenames
+  /Base[A-Z]\w+\.(vue|js)$/
+);
+
+requireComponent.keys().forEach((fileName) => {
+  // Get component config
+  const componentConfig = requireComponent(fileName);
+
+  // Get PascalCase name of component
+  const componentName = upperFirst(
+    camelCase(
+      // Gets the file name regardless of folder depth
+      fileName
+        .split("/")
+        .pop()
+        .replace(/\.\w+$/, "")
+    )
+  );
+
+  // Register component globally
+  Vue.component(
+    componentName,
+    // Look for the component options on `.default`, which will
+    // exist if the component was exported with `export default`,
+    // otherwise fall back to module's root.
+    componentConfig.default || componentConfig
+  );
+});
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount("#app");
